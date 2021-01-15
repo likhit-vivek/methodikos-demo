cd2b6781878229093c21fd250cafebf960474c2c
vivek <vivekguf1509@gmail.com>
2021-01-15 23:23:10 +0530

    Added report window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,51 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'infraframe-demo';
 
+  constructor(
+    public dialog: MatDialog
+  ) {}
+
+  openReportDialog() {
+    const dialogRef = this.dialog.open(ReportDialogComponent, {
+      'width': '500px',
+      autoFocus: false,
+    });
+  }
+
   logout() {
     window.location.href = 'https://infraframe.com/';
+  }
+}
+
+@Component({
+  selector: 'report-dialog',
+  templateUrl: './report-dialog.component.html',
+  styleUrls: ['./report-dialog.component.sass', './app.component.sass']
+})
+export class ReportDialogComponent {
+
+  workPlanLink: any = "https://drive.google.com/uc?export=download&id=1M57zTjbbRCwpboyG0fgIwPOOmsy24DDB";
+  networkReportLink: any = "https://drive.google.com/uc?export=download&id=1NfwGT5KXLdKzuap9uupnqut8CZKgYe66";
+  segmentReportLink: any = "https://drive.google.com/uc?export=download&id=1mAkzPEdPNxQt94Q6aXwnrM023qAQOWaA";
+  csvLink: any = "https://drive.google.com/uc?export=download&id=1vDVG1ZUI5_thbC7a1jFEg6Pz22uAp5SR";
+
+  constructor(
+    public dialogRef: MatDialogRef<ReportDialogComponent>,
+  ) {
+    this.dialogRef.backdropClick().subscribe(_ => {
+      this.close();
+    });
+  }
+
+  downloadLink(param) {
+    if(param == 'workplan') var win = window.open(this.workPlanLink, "_blank");
+    if(param == 'networkreport') var win = window.open(this.networkReportLink, "_blank");
+    if(param == 'segmentreport') var win = window.open(this.segmentReportLink, "_blank");
+    if(param == 'csv') var win = window.open(this.csvLink, "_blank");
+    win.focus();
+  }
+
+  close() {
+    this.dialogRef.close();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, ReportDialogComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule, MatDividerModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportDialogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatDividerModule
+  ],
+  entryComponents: [
+    ReportDialogComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
